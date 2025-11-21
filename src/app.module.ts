6b2ea7b7123de261aa518ef01d804f25363271ca
Angel Penalver <apenalver4@gmail.com>
2025-11-21 13:06:40 -0400

    fix: updating app.module for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,14 @@ import { CategoriesModule } from './categories/categories.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL, 
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, 
+      
+      ssl: process.env.STAGE === 'prod' ? {
+        rejectUnauthorized: false
+      } : false, 
+      
     }),
     UserModule,
     AuthModule,
